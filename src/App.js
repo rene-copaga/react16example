99fe3d4d3f11dd436d34b3ed52a6e09df47a9322
Rene Copaga <rene.copaga@gmail.com>
2020-12-21 10:08:20 -0400

    Using the Data Store in the React Application -Applying the Data Store to the Top-Level Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
 import ProductsAndSuppliers from './ProductsAndSuppliers';
+import dataStore from './store';
 
 export default class App extends Component {
 
   render() {
-    return <ProductsAndSuppliers />
+    return (
+      <Provider store={ dataStore }>
+        <ProductsAndSuppliers />
+      </Provider>
+    )
   }
 }
