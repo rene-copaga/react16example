8a4e4db01df407d2426c609fa90d1a25d423a9b7
Rene Copaga <rene.copaga@gmail.com>
2020-12-21 11:57:38 -0400

    -Incorporating the State Data Features into the Store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,13 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 import modelReducer from "./modelReducer";
+import stateReducer from "./stateReducer"
 
-export default createStore(modelReducer);
+export default createStore(combineReducers(
+    {
+        modelData: modelReducer,
+        stateData: stateReducer
+    }
+));
 
 export { saveProduct, saveSupplier, deleteProduct, deleteSupplier }
     from "./modelActionCreators";
--- src/store/modelReducer.js
+++ src/store/modelReducer.js
@@ -22,6 +22,6 @@ export default function (storeData, action) {
                     .filter(p => p.id !== action.payload)
             }
         default:
-            return storeData || initialData;
+            return storeData || initialData.modelData;
     }
 }
