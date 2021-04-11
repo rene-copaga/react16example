72415f89e42f2094beeecac446f49c70a774e212
Rene Copaga <rene.copaga@gmail.com>
2021-04-10 21:23:22 -0400

    -Creating a Connector Component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,19 @@ import { Selector } from './Selector';
 import dataStore from './store';
 import { SupplierDisplay } from './SupplierDisplay';
 import { StoreAccess } from "./store/StoreAccess";
+import { CustomConnector, CustomConnectorProvider } from "./store/CustomConnector";
+import { startEditingProduct } from "./store/stateActions";
+import { ProductTable } from "./ProductTable";
+import { deleteProduct } from "./store/modelActionCreators";
 
+const selectors = {
+  products: (store) => store.modelData.products
+}
+
+const dispatchers = {
+  editCallback: startEditingProduct,
+  deleteCallback: deleteProduct
+}
 export default class App extends Component {
 
   render() {
@@ -21,6 +33,16 @@ export default class App extends Component {
               <SupplierDisplay name="Suppliers" />
             </Selector>
           </Provider>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <CustomConnectorProvider dataStore={dataStore}>
+            <CustomConnector selectors={selectors}
+              dispatchers={dispatchers}>
+              <ProductTable />
+            </CustomConnector>
+          </CustomConnectorProvider>
         </div>
       </div>
     </div>
