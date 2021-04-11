2e36d75897a04281ac881b13d294344bd2bc517f
Rene Copaga <rene.copaga@gmail.com>
2021-04-11 16:22:01 -0400

    Enhancing Reducers

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,17 @@
 import { combineReducers, createStore } from "redux";
+import { customReducerEnhancer } from "./customReducerEnhancer";
 import modelReducer from "./modelReducer";
 import stateReducer from "./stateReducer"
 
-export default createStore(combineReducers(
-    {
-        modelData: modelReducer,
-        stateData: stateReducer
-    }
-));
+const enhancedReducer = customReducerEnhancer(
+    combineReducers(
+        {
+            modelData: modelReducer,
+            stateData: stateReducer
+        })
+);
+
+export default createStore(enhancedReducer);
 
 export { saveProduct, saveSupplier, deleteProduct, deleteSupplier }
     from "./modelActionCreators";
