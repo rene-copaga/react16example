a8a5bd5dcc8500417c38fb55c59ef4da7a297f57
Rene Copaga <rene.copaga@gmail.com>
2021-04-12 17:53:03 -0400

    Using Data Store Middleware fat arrow function

--- src/store/multiActionMiddleware.js
+++ src/store/multiActionMiddleware.js
@@ -1,11 +1,7 @@
-export function multiActions({dispatch, getState}) {
-    return function receiveNext(next) {
-        return function processAction(action) {
-            if (Array.isArray(action)) {
-                action.forEach(a => next(a));
-            } else {
-                next(action);
-            }
-        }
+export const multiActions = ({ dispatch, getState }) => next => action => {
+    if (Array.isArray(action)) {
+        action.forEach(a => next(a));
+    } else {
+        next(action);
     }
 }
