3bfa632798b6ce88677a646d8743636d29db797e
Rene Copaga <rene.copaga@gmail.com>
2021-04-12 19:32:52 -0400

    Enhancing the Data Store fat arrow function

--- src/store/asyncEnhancer.js
+++ src/store/asyncEnhancer.js
@@ -1,16 +1,12 @@
-export function asyncEnhancer(delay) {
-    return function (createStoreFunction) {
-        return function (...args) {
-            const store = createStoreFunction(...args);
-            return {
-                ...store,
-                dispatchAsync: (action) => new Promise((resolve, reject) => {
-                    setTimeout(() => {
-                        store.dispatch(action);
-                        resolve();
-                    }, delay);
-                })
-            };
-        }
-    }
+export const asyncEnhancer = delay => createStoreFunction => (...args) => {
+    const store = createStoreFunction(...args);
+    return {
+        ...store,
+        dispatchAsync: (action) => new Promise((resolve, reject) => {
+            setTimeout(() => {
+                store.dispatch(action);
+                resolve();
+            }, delay);
+        })
+    };
 }
