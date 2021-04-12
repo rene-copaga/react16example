36616fb66ff56531162bcaf4c34246cc8a416f3f
Rene Copaga <rene.copaga@gmail.com>
2021-04-12 19:55:11 -0400

    Using the React-Redux API -Advanced Connect Features --Mapping Data Props

--- src/store/TableConnector.js
+++ src/store/TableConnector.js
@@ -5,15 +5,21 @@ import { PRODUCTS, SUPPLIERS } from "./dataTypes";
 
 export const TableConnector = (dataType, presentationComponent) => {
 
-    const mapStateToProps = (storeData) => ({
-        products: storeData.modelData[PRODUCTS],
-        suppliers: storeData.modelData[SUPPLIERS].map(supp => ({
-            ...supp,
-            products: supp.products.map(id =>
-                storeData.modelData[PRODUCTS].find(p => p.id === Number(id)) || id)
-                    .map(val => val.name || val)
-        }))
-    })
+    const mapStateToProps = (storeData, ownProps) => {
+        if (!ownProps.needSuppliers) {
+            return { products: storeData.modelData[PRODUCTS] };
+        } else {
+            return {
+                suppliers: storeData.modelData[SUPPLIERS].map(supp => ({
+                    ...supp,
+                    products: supp.products.map(id =>
+                        storeData.modelData[PRODUCTS]
+                            .find(p => p.id === Number(id)) || id)
+                        .map(val => val.name || val)
+                }))
+            }
+        }
+    }
 
     const mapDispatchToProps = {
         editCallback: dataType === PRODUCTS
