ea6e0775f9026931c37cee7729c104e99ec177b8
Rene Copaga <rene.copaga@gmail.com>
2021-04-14 20:05:42 -0400

    -Matching URLs --Matching Using Segments

--- src/Selector.js
+++ src/Selector.js
@@ -5,25 +5,22 @@ import { SupplierDisplay } from "./SupplierDisplay";
 
 export class Selector extends Component {
 
+    renderMessage = (msg) => <h5 className="bg-info text-white m-2 p-2">{msg}</h5>
+
     render() {
         return <Router>
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-2">
-                        <div><Link to="/products">Products</Link></div>
-                        <div><Link to="/suppliers">Suppliers</Link></div>
+                        <div><Link to="/data/one">Link #1</Link></div>
+                        <div><Link to="/data/two">Link #2</Link></div>
+                        <div><Link to="/people/bob">Bob</Link></div>
                     </div>
                     <div className="col">
-                        <Route path="/products" render={(routeProps) =>
-                            <ProductDisplay myProp="myValue" />} />
-                        <Route path="/suppliers" render={(routeProps) =>
-                            <React.Fragment>
-                                <h4 className="bg-info text-center text-white p-2">
-                                    Suppliers
-                                </h4>
-                                <SupplierDisplay />
-                            </React.Fragment>
-                        } />
+                        <Route path="/data"
+                            render={() => this.renderMessage("Route #1")} />
+                        <Route path="/data/two"
+                            render={() => this.renderMessage("Route #2")} />
                     </div>
                 </div>
             </div>
