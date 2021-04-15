f3afb3590133bfb4d6b288be5e731fea89cac714
Rene Copaga <rene.copaga@gmail.com>
2021-04-14 22:16:25 -0400

    -Making a Single Route Match

--- src/Selector.js
+++ src/Selector.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import { ProductDisplay } from "./ProductDisplay";
 import { SupplierDisplay } from "./SupplierDisplay";
 
@@ -12,16 +12,17 @@ export class Selector extends Component {
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-2">
-                        <div><Link to="/data">Data</Link></div>
-                        <div><Link to="/data/one">Link #1</Link></div>
-                        <div><Link to="/data/two">Link #2</Link></div>
-                        <div><Link to="/data/three">Link #3</Link></div>
-                        <div><Link to="/people/bob">Bob</Link></div>
-                        <div><Link to="/people/alice">Alice</Link></div>
+                        <div><Link to="/">Default URL</Link></div>
+                        <div><Link to="/products">Products</Link></div>
+                        <div><Link to="/suppliers">Suppliers</Link></div>
                     </div>
                     <div className="col">
-                        <Route path={["/data/(one|three)", "/people/b*"]}
-                            render={() => this.renderMessage("Route #1")} />
+                        <Switch>
+                            <Route path="/products" component={ProductDisplay} />
+                            <Route path="/suppliers" component={SupplierDisplay} />
+                            <Route render={() =>
+                                this.renderMessage("Fallback Route")} />
+                        </Switch>
                     </div>
                 </div>
             </div>
