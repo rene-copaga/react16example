d4fbae0213d27ac0b8ba5420089a043e009767ba
Rene Copaga <rene.copaga@gmail.com>
2021-04-17 14:52:59 -0400

    -Saving, Updating, and Deleting Data

--- src/webservice/RestDataSource.js
+++ src/webservice/RestDataSource.js
@@ -10,10 +10,27 @@ export class RestDataSource {
         this.SendRequest("get", this.BASE_URL, callback);
     }
 
-    async SendRequest(method, url, callback) {
+    async GetOne(id, callback) {
+        this.SendRequest("get", `${this.BASE_URL}/${id}`, callback);
+    }
+
+    async Store(data, callback) {
+        this.SendRequest("post", this.BASE_URL, callback, data)
+    }
+
+    async Update(data, callback) {
+        this.SendRequest("put", `${this.BASE_URL}/${data.id}`, callback, data);
+    }
+
+    async Delete(data, callback) {
+        this.SendRequest("delete", `${this.BASE_URL}/${data.id}`, callback, data);
+    }
+    
+    async SendRequest(method, url, callback, data) {
         callback((await Axios.request({
             method: method,
-            url: url
+            url: url,
+            data: data
         })).data);
     }
 }
